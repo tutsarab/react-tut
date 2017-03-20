32c73e39faca7481e8e25fa02ff11d3be3adc303
Roman Alshehri <aalshehri08@hotmail.com>
2017-03-20 19:57:51 -0400

    Fixing webpack.config.js

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,43 +1,31 @@
-'use strict'
-var path = require('path');
+var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
-	context: path.join(__dirname),
-	entry: './src/entry.js', // I can make two entry points
-	output: {
-		path: path.join(__dirname),
-		filename: "bundle.js"
-	},
-	devtool: 'source-map',
-	preloaders: [
-		{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "jshint-loader"
-		}
-	],
-	module: {
-		loaders: [
-            {
-                test: /\.jsxZ$/,
-                exclude: /node_modules/,
-                loader: /babel/
-            },{
-				test: /\.css$/,
-				loader: 'style!css!'
-			},{
-				test: /\.html$/,
-				loader: 'raw'
-			}
-
-		]
-	},
-	plugins: [
-
-		new webpack.optimize.CommonsChunkPlugin("common.js")
-	],
-	externals: {
-
-	}
+  context: path.join(__dirname, "src"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/client.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-decorators-legacy', 'transform-class-properties'],
+        }
+      }
+    ]
+  },
+  output: {
+    path: __dirname + "/src/",
+    filename: "client.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
 };
